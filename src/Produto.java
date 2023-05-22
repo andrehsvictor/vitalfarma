d539edc846b6681c7909e636e0f461078298deae
André Victor <andrehsvictor@gmail.com>
2023-05-22 15:57:56 -0300

    Adiantando uns bglh

---
+++ src/Produto.java
@@ -0,0 +1,27 @@
+
+public abstract class Produto {
+	private int id;
+	private double preco;
+	private String tipo;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public double getPreco() {
+		return preco;
+	}
+	public void setPreco(double preco) {
+		this.preco = preco;
+	}
+	public String getTipo() {
+		return tipo;
+	}
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+
+	
+}
---
+++ src/RepositorioDeProdutos.java
@@ -0,0 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class RepositorioDeProdutos {
+	private List<Produto> produtos = new ArrayList<>();
+}

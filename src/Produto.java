82f4d5eb3e12075454ca378409a4f4172db083da
Suerdo Flaubert <120109004+Suerdo@users.noreply.github.com>
2023-05-26 17:08:58 -0300

    Update Produto.java

--- src/Produto.java
+++ src/Produto.java
@@ -1,4 +1,28 @@
 
-public class Produto {
+public abstract class Produto {
+    private String nome;
+    private double preco;
 
+    public abstract double calcularValor();
+
+    public Produto(String nome, double preco) {
+        this.nome = nome;
+        this.preco = preco;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
 }

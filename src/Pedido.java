9c05f350d7377d91cb3a8f09668bbc3a79cb66fc
Maria Victória <126691192+VictoriAraujo@users.noreply.github.com>
2023-05-28 09:22:38 -0300

    Add files via upload

---
+++ src/Pedido.java
@@ -0,0 +1,36 @@
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+
+import java.util.List;
+public class Pedido {
+    private List<Produto> produtos;
+    private double valorTotal;
+    private LocalDateTime dataHora;
+    public Pedido() {
+        this.produtos = new ArrayList<>();
+        this.valorTotal = 0.0;
+        dataHora = LocalDateTime.now();
+    }
+
+    public void adicionarProduto(Produto produto) {
+        produtos.add(produto);
+    }
+
+    public void removerProduto(Produto produto) {
+        produtos.remove(produto);
+    }
+
+    public double calcularValorTotal() {
+        valorTotal = 0.0;
+        for (Produto produto : produtos) {
+            valorTotal += produto.calcularValor();
+        }
+        return valorTotal;
+    }
+    public String dataToString() {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
+        return dataHora.format(formatter);
+    }
+}

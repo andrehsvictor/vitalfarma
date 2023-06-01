d151a5945bf8ea9d01aee02044bf2a2d8cc481b4
Maria Victória <126691192+VictoriAraujo@users.noreply.github.com>
2023-05-31 21:13:47 -0300

    Update Cosmetico.java

--- src/Cosmetico.java
+++ src/Cosmetico.java
@@ -1,33 +1,53 @@
 public class Cosmetico extends Produto {
-    public Cosmetico(String nome) {
-        super(nome, definirPreco(nome));
-    }
-    
-    private static double definirPreco(String nome) {
-    	final double PRECO_CREME_PARA_PELE = 29.90,
-    			PRECO_HIDRATANTE = 19.90,
-    			PRECO_PROTETOR_SOLAR = 39.90,
-    			PRECO_OLEO_DE_PELE = 24.90;
-    	
-    	// Formata pra minúsculo
-    	String nomeFormatado = nome.toLowerCase();
-    	
-        switch (nomeFormatado) {
-            case "creme para pele":
-                return PRECO_CREME_PARA_PELE;
-            case "hidratante":
-                return PRECO_HIDRATANTE;
-            case "protetor solar":
-                return PRECO_PROTETOR_SOLAR;
-            case "óleo de pele":
-                return PRECO_OLEO_DE_PELE;
-            default:
-                throw new ProdutoInvalidoException("Cosmético inválido: " + nome);
+
+    private String nome;
+
+    public enum TipoCosmetico {
+        CREME_PARA_PELE("Creme para Pele", 29.90),
+        HIDRATANTE("Hidratante", 19.90),
+        PROTETOR_SOLAR("Protetor Solar", 39.90),
+        OLEO_DE_PELE("Óleo de Pele", 24.90);
+
+        private String nome;
+        private double preco;
+
+        TipoCosmetico(String nome, double preco) {
+            this.nome = nome;
+            this.preco = preco;
+        }
+
+        public String getNome() {
+            return nome;
+        }
+
+        public double getPreco() {
+            return preco;
         }
     }
 
-    @Override
-    public double calcularValor() {
-        return getPreco();
+    public Cosmetico(String nome) {
+        super(nome, 0);
+        this.nome = nome;
+        setPreco(calcularValor(nome));
     }
+
+    @Override
+    public double calcularValor(String nome) {
+        String nomeFormatado = this.nome.toLowerCase();
+
+        for (TipoCosmetico tipo : TipoCosmetico.values()) {
+            if (nomeFormatado.equals(tipo.getNome().toLowerCase())) {
+                return tipo.getPreco();
+            }
+        }
+
+        throw new ProdutoInvalidoException("Cosmético inválido: " + this.nome);
+
+    }
+
+    @Override
+    public String getDescricao() {
+        return "Nome do Cosmético: "+ getNome() + "\nValor: " +getPreco();
+    }
+
 }

9ea9ac5d7cacb1d95fec6b4913a5a61f52b9e045
Maria Victória <126691192+VictoriAraujo@users.noreply.github.com>
2023-05-31 21:24:39 -0300

    Update CosmeticoTest.java

--- tests/CosmeticoTest.java
+++ tests/CosmeticoTest.java
@@ -1,19 +1,46 @@
-import static org.junit.jupiter.api.Assertions.*;
-
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-class CosmeticoTest {
-	
-	@Test
-	void testCalcularValor() {
-		Cosmetico cosmetico = new Cosmetico("Hidratante");
-		double preco = cosmetico.calcularValor();
-		assertEquals(19.90, preco);
-	}
+public class TesteCosmetico {
 
-	@Test
-	void testCosmetico() {
-		fail("Not yet implemented");
-	}
+    @Test
+    public void testes() {
+        Cosmetico creme = new Cosmetico("Creme para Pele");
+        double valorEsperado = 29.90;
+        Assertions.assertEquals(valorEsperado, creme.getPreco());
 
+    }
+
+    @Test
+    public void testeProtetorSolar() {
+        Cosmetico cosmetico = new Cosmetico("Protetor Solar");
+        double valorEsperado = 39.90;
+        Assertions.assertEquals(valorEsperado, cosmetico.getPreco());
+    }
+    @Test
+    public void testeHidratante() {
+        Cosmetico cosmetico = new Cosmetico("Hidratante");
+        double valorEsperado = 19.90;
+        Assertions.assertEquals(valorEsperado, cosmetico.getPreco());
+    }
+    @Test
+    public void testeOleoDePele() {
+        Cosmetico cosmetico = new Cosmetico("Óleo de Pele");
+        double valorEsperado = 24.90;
+        Assertions.assertEquals(valorEsperado, cosmetico.getPreco());
+
+    }
+
+    @Test
+    public void testeGetDescricao() {
+        Cosmetico cosmetico = new Cosmetico("Creme para Pele");
+        String descricaoEsperada = "Nome do Cosmético: Creme para Pele\nValor: 29.9";
+        Assertions.assertEquals(descricaoEsperada, cosmetico.getDescricao());
+    }
+    @Test
+    public void testeExceção() {
+        Assertions.assertThrows(ProdutoInvalidoException.class, () -> {
+            Cosmetico cosmetico = new Cosmetico("Sabonete");
+        });
+    }
 }

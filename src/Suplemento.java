7c30a12f1f75fd40fe5a67088802344ed353b692
Maria Victória <126691192+VictoriAraujo@users.noreply.github.com>
2023-05-31 22:25:46 -0300

    Add files via upload

--- src/Suplemento.java
+++ src/Suplemento.java
@@ -1,27 +1,49 @@
 public class Suplemento extends Produto {
-    public Suplemento(String nome) {
-        super(nome, definirPreco(nome));
-    }
 
-    private static double definirPreco(String nome) {
-        switch (nome.toLowerCase()) {
-            case "creatina":
-                return 120.00;
-            case "proteina":
-                return 99.99;
-            case "vitamina":
-                return 70.60;
-            case "ômega 3":
-                return 55.30;
-            case "colágeno":
-                return 105.00;
-            default:
-                throw new IllegalArgumentException("Supremento inválido: " + nome);
+    public enum TipoSuplemento{
+        CREATINA("Creatina", 120),
+        PROTEINA("Proteina", 95.90),
+        VITAMINA("Vitamina", 70.50),
+        ÔMEGA3("Ômega 3", 55.40),
+        COLÁGENO("Colágeno", 105.00);
+
+        private String nome;
+        private double preco;
+
+        TipoSuplemento(String nome, double preco) {
+            this.nome = nome;
+            this.preco = preco;
         }
+
+        public String getNome() {
+            return nome;
+        }
+
+        public double getPreco() {
+            return preco;
+        }
+    }
+    public Suplemento(String nome) {
+        super(nome, 0);
+
+        setPreco(calcularValor(nome));
     }
 
     @Override
-    public double calcularValor() {
-        return getPreco();
+    public double calcularValor(String nome) {
+        String nomeFormatado = nome.toLowerCase();
+
+        for (Suplemento.TipoSuplemento tipo : Suplemento.TipoSuplemento.values()) {
+            if (nomeFormatado.equals(tipo.getNome().toLowerCase())) {
+                return tipo.getPreco();
+            }
+        }
+
+        throw new ProdutoInvalidoException("Suplemento inválido: " + getNome());
+    }
+
+    @Override
+    public String getDescricao() {
+        return "Nome do Suplemento: "+ getNome() + "\nValor: " +getPreco();
     }
 }

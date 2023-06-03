a2dd23fd1fb0494867066b7f60a5125b497e8f37
André Victor <andrehsvictor@gmail.com>
2023-06-03 11:03:08 -0300

    Update Main.java

--- main/Main.java
+++ main/Main.java
@@ -1,12 +1,13 @@
-
 public class Main {
 
 	public static void main(String[] args) {
 		VitalFarma vf = new VitalFarma();
 		vf.getEstoque()
-		.listarProdutos();
-		vf.getEstoque().removerSuplementoPorNome("vitamina");
-		vf.getEstoque().listarProdutos();
+		  .listarProdutos();
+		vf.getEstoque()
+		  .removerSuplementoPorNome("vitamina");
+		vf.getEstoque()
+		  .listarProdutos();
 	}
 
 }

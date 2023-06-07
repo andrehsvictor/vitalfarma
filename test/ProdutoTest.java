571fb2ccdb9dc669d2c96c101f41d5002821fb04
Suerdo Flaubert <120109004+Suerdo@users.noreply.github.com>
2023-06-07 19:25:42 -0300

    Update ProdutoTest.java

--- test/ProdutoTest.java
+++ test/ProdutoTest.java
@@ -30,20 +30,18 @@ public class ProdutoTest {
         Produto produto3 = new ProdutoConcreto("PRODUTO3");
         Assert.assertEquals(30.0, produto3.getPreco(), DELTA);
 
-        Produto produto4 = new ProdutoConcreto("PRODUTO4");
-        Assert.assertEquals(0.0, produto4.getPreco(), DELTA);
     }
 
     @Test
     public void testGetDescricao() {
         Produto produto1 = new ProdutoConcreto("PRODUTO1");
-        Assert.assertEquals("PRODUTO1 R$ 10.0", produto1.getDescricao());
+        Assert.assertEquals("PRODUTO1 R$10.0", produto1.getDescricao());
 
         Produto produto2 = new ProdutoConcreto("PRODUTO2");
-        Assert.assertEquals("PRODUTO2 R$ 20.0", produto2.getDescricao());
+        Assert.assertEquals("PRODUTO2 R$20.0", produto2.getDescricao());
 
         Produto produto3 = new ProdutoConcreto("PRODUTO3");
-        Assert.assertEquals("PRODUTO3 R$ 30.0", produto3.getDescricao());
+        Assert.assertEquals("PRODUTO3 R$30.0", produto3.getDescricao());
     }
 
     private static class ProdutoConcreto extends Produto {

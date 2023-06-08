d21dde2254f133776a812a11ffab017284759655
André Victor <andrehsvictor@gmail.com>
2023-06-08 11:12:28 -0300

    Adicionei o método isSujeitoAPrescricao()
    Ele verifica se o remédio é sujeito a prescrição médica

--- produtos/Remedio.java
+++ produtos/Remedio.java
@@ -10,8 +10,29 @@ public class Remedio extends Produto {
         getMapaDePrecos().put("HISTAMIN", 8.70);
         getMapaDePrecos().put("LOSARTANA", 32.70);
     }
-        public Remedio(String nome) {
+    
+    public Remedio(String nome) {
             super(nome);
     }
-
+    
+    private boolean isSujeitoAPrescricao(String nomeDoRemedio) {
+    	switch (nomeDoRemedio) {
+    	case "RIVOTRIL":
+    		return true;
+    	case "LOSARTANA":
+    		return true;
+    	case "NEOSORO":
+    		return true;
+    	default:
+    		return false;
+    	}
+    } 
+    
+    @Override
+    public void exibirDescricao() {
+    	if(isSujeitoAPrescricao(getNome()))
+    		System.out.printf("%-20s R$%-2.2f [Sujeito a prescricao medica]%n", getNome(), getPreco());
+    	else
+    		super.exibirDescricao();;
+    }
 }
